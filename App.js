e0655bc2bc333a16c5e44168f74861c0bdbe8739
pcw216 <pwinters@rallydev.com>
2014-06-06 19:24:23 -0400

    More skeleton stuffs. npm and bower. a bunch of skeleton services and what not

--- App.js
+++ App.js
@@ -3,13 +3,81 @@ Ext.define('CustomApp', {
     componentCls: 'app',
     items:{ html:''},
     launch: function() {
+        debugger
         //Write app code here
         angular.bootstrap(document.body, ['insights.movers']);
+        var scope = angular.element(document.body).scope();
+        scope.app = this.getContext().map.map;
+        scope.$digest();
     }
 });
 
 var module = angular.module('insights.movers', []);
-module.controller('RootCtrl', function($scope){
-	$scope.test = 'my value';
-	console.log('test');
-})
+module.run(function($rootScope){
+	$rootScope.app = $rootScope.app || {};
+	$rootScope.app.scorecard = 'balanced';
+});
+
+module.controller('RootCtrl', function($scope, insightsApi, projectLoader){
+
+	$scope.$watch('app.project', function(project){
+		if(project) {
+			projectLoader.getScopedProjects($scope.app.project, $scope.app.workspace).then(function(projects){
+				return insightsApi.loadData($scope.app.scorecard, projects)
+			}).then(function(data){
+				$scope.data = data
+			});
+		}
+	})
+
+});
+
+module.service('variationCalculator', function(){
+
+	// For each project, calculate a variation against the workspace
+	this.calculateVariationForProject = function(projectData){
+
+	};
+
+	// Array of data objects for projects
+	this.createHighChartsSeries = function(projectsData){
+
+	};
+
+
+});
+
+module.service('projectLoader', function($http, $q){
+	
+	// Walk the alm project children tree to get all project/workspace oids
+	this.getScopedProjects = function(rootProject, workspace){
+		
+		// TODO Load the projects. yay.
+		return $q.when([{projectId:rootProject.ObjectID, workspaceId: workspace.ObjectID}]);
+	};
+
+});
+
+module.service('insightsApi', function($http, $q){
+
+	// Sample Query
+	// https://rally1.rallydev.com/insight/scorecardData?end-date=2014-06&granularity=quarter&projectId=7427420584&scorecardConfigId=balanced_all_visible&start-date=2013-05&workspaceId=41529001
+	this.loadData = function(scorecard, projects){
+		return $q.all(_.each(projects, function(project){
+			var params = {
+				'scorecardConfigId': 'balanced',
+				'start-date': '2014-01', // TODO Base these dates on current Now() - 1 month
+				'end-date': '2014-01',
+				'granularity': 'month',
+				'projectId': project.projectId,
+				'workspaceId': project.workspaceId
+
+			};
+			return $http({
+				url: 'https://rally1.rallydev.com/insight/scorecardData',
+				method: 'GET',
+				params: params
+			});
+		}))
+	};
+});
