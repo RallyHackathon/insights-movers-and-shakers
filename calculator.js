e02c19638943d252d7242c1e266ebe5284987a30
pcw216 <pwinters@rallydev.com>
2014-06-07 09:23:21 -0400

    Separate calculator script

---
+++ calculator.js
@@ -0,0 +1,21 @@
+angular.module('insights.movers.calculator', []).service('variationCalculator', function(){
+
+	// For each project, calculate a variation against the workspace
+	this.calculateVariationForProject = function(projectData){
+		// 
+
+		// _(responses)
+		// .map(function(response){_.first(response.scopes)}) // Get project scopes
+		// .map(function(scope){return scope.dataPoints}) // Get data points
+		// .pluck('Quality')
+		// .pluck('score')
+
+	};
+
+	// Array of data objects for projects
+	this.createHighChartsSeries = function(projectsData){
+
+	};
+
+
+});
